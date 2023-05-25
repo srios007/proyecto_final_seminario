1c5f76cb0e431be1f515985be1249e8d622c6ec9
Santiago Rios Valero <sriosv@correo.udsitrital.edu.co>
2023-05-24 22:15:46 -0500

    [ADD] change colors

--- lib/app/utils/palette.dart
+++ lib/app/utils/palette.dart
@@ -5,11 +5,11 @@ import 'package:flutter/material.dart';
 ///
 
 class Palette {
-  static const purple = Color(0xFF5954D2);
+  static const purple = Color(0xFFF25C05);
   static const darkBlue = Color(0xFF0A2F5B);
   static const lightBlue = Color(0xFF3478D7);
   static const yellow = Color(0xFFFEF900);
-  static const green = Color(0xFF5EE8C1);
+  static const green = Color.fromARGB(255, 246, 228, 117);
   static const white = Color(0xFFFFFFFF);
   static const gray = Color(0xFF696767);
   static const lightGray = Color(0xFFB1B1B1);
